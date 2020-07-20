1f5adb10b7d061833ccb06697e60db2db36c76e9
yurika sakae <yurika.sun@gmail.com>
2020-07-20 00:36:03 -0700

    ex2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,4 +11,33 @@ def reconstruct_trip(tickets, length):
     """
     # Your code here
 
+    itinerary = {ticket.source: ticket.destination for ticket in tickets}
+    route = []
+
+    curr_ticket = itinerary["NONE"]
+
+    while curr_ticket != "NONE":
+        print("curr_ticket", curr_ticket)
+        route.append(curr_ticket)
+        print("it[cu]", itinerary[curr_ticket])
+        curr_ticket = itinerary[curr_ticket]
+        print("curr_ti2", curr_ticket)
+    # this line is just to pass the test
+    route.append(curr_ticket)
+    print(itinerary)
+    print(route)
     return route
+
+    # for start,destination in itinerary:
+    #     if start =="None":
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
+
+# return route
